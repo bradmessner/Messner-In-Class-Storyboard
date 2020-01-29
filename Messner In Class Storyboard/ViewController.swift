1c23055faf5a75bd25d40a936251abea2df56e6e
Brad D Messner <bmessner@setonhill.edu>
2020-01-29 09:36:59 -0500

    Initial Commit

---
+++ Messner In Class Storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messner In Class Storyboard
+//
+//  Created by Brad D. Messner on 1/29/20.
+//  Copyright © 2020 Brad D. Messner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Messner In Class StoryboardTests/Messner_In_Class_StoryboardTests.swift
@@ -0,0 +1,34 @@
+//
+//  Messner_In_Class_StoryboardTests.swift
+//  Messner In Class StoryboardTests
+//
+//  Created by Brad D. Messner on 1/29/20.
+//  Copyright © 2020 Brad D. Messner. All rights reserved.
+//
+
+import XCTest
+@testable import Messner_In_Class_Storyboard
+
+class Messner_In_Class_StoryboardTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
