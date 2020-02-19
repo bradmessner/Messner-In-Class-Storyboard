fd7600a3fe891de03c738a43b39d419b2f0b9a29
Brad D Messner <bmessner@setonhill.edu>
2020-02-19 10:04:33 -0500

    Ready

--- Messner In Class Storyboard/ViewController.swift
+++ Messner In Class Storyboard/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var nameField : String = "My name is <name> and I really <verb> a lot. Mr. Messner is my <adjective> favorite professor."
+    
+    var madlibTemplate : String = "Date: <date> Please excuse <name> who is far too <adjective> to attend <noun>."
+    
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var outputText: UITextField!
+    
+     @IBAction func pressButton(_ sender: Any) {
+       
+        outputText.text = inputText.text
+        outputText.text = nameField.replacingOccurrences(of: "<name>", with: inputText.text!)
+
+        outputText.text = madlibTemplate.replacingOccurrences(of: "<name>", with: inputText.text!)
+        
+        
+        // Note that you would need inputField.text.replacingOccurences(....) to copy from
+        // a specific textfield instead of just variable contents
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
